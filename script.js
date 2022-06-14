e13d3aa134e7bc0d33e261741b6e788bb6b697c3
luanaparra <luana.silva@sou.inteli.edu.br>
2022-06-14 10:17:58 -0300

    update

--- script.js
+++ script.js
@@ -1,34 +1,104 @@
-//JavaScript
-//const elements = {
-//    academic: document.querySelectorAll(".academic")
-//}
-
-//elements.academic.forEach(element => {
-//    element.addEventListener("click", function(e) {
-//        if(!element.classList.contains("active")) {
-//            element.children[1].style.display = "initial";
-//            element.classList.add("active");
-//            element.children[0].children[1].src="./plus-solid.svg"
-//        } else {
-//            element.children[1].style.display = "none";
-//            element.classList.remove("active");
-//            element.children[0].children[1].src="./plus-solid.svg"
-//        }
-//    })
-//})
-
-//jQuery
-$(".academic").each(function(index) {
-    $(this).on( "click", function(e) {
-        if(!$(this).hasClass("active")) {
-            $(this)[0].children[1].style.display = "initial";
-            $(this).addClass("active");
-            $(this)[0].children[0].children[1].src="./plus-solid.svg"
-        } else {
-            $(this)[0].children[1].style.display = "none";
-            $(this).removeClass("active");
-            $(this)[0].children[0].children[1].src=="./plus-solid.svg"
-        }
+﻿// USADO COM app_713.js
+api = 'https://andre-braga-app-713.herokuapp.com';
+$(document).ready(() => {
+    $("#bt1").click(function(e){
+        usersx.list();
     });
-
-})
+    $("#bt2").click(
+        function(e){
+            users.list();
+        }        
+    );
+});
+var users = {   
+    list() {
+        $.ajax({
+            url: api + '/users',
+            type: 'GET',
+            success: data => {
+                var tx = '';
+                tx += '<div class="insert" onclick="user.insert()">Inserir</div>';
+                data.forEach(element => {
+                    tx += '<div class="user">';
+                        tx += '<div class="title">' + element.title + '</div>';
+                        tx += '<div class="actions">';
+                            tx += '<div class="action" onclick="user.update(' + element.userId + ',\'' + element.title + '\')">Editar</div>';
+                            tx += '<div class="action" onclick="user.delete(' + element.userId + ')">Excluir</div>';
+                        tx += '</div>';
+                    tx += '</div>';
+                });
+                $('#LISTA').html(tx);
+            }
+        });        
+    }
+};
+var usersx = {
+    list() {
+        $.ajax({
+            url: api + '/users',
+            type: 'GET',
+            success: data => {
+                var tx = '';
+                data.forEach(element => {
+                        tx += '<div class="user">';
+                        tx += '<div class="title">' + element.title + '</div>';
+                        tx += '</div>';
+                });
+                $('#LISTA').html(tx);
+            }
+        });        
+    }    
+};
+var user = {
+    insert() {
+        var title = prompt('Digite o nome:');
+        if (title) {
+            if (title.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/userinsert',
+                    data: {title: title},
+                }).done(function () {
+                    users.list();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    },
+    update(userId, oldTitle) {
+        var title = prompt('Digite o novo nome:', oldTitle);
+        if (title) {
+            if (title.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/userupdate',
+                    data: {title: title, userId: userId},
+                }).done(function () {
+                    users.list();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    },
+    delete(userId) {
+        if (confirm('Confirma a exclusão?')) {
+            $.ajax({
+                type: 'POST',
+                url: api + '/userdelete',
+                data: {userId: userId},
+            }).done(function () {
+                users.list();
+            }).fail(function (msg) {
+                console.log('FAIL');
+            }).always(function (msg) {
+                //console.log('ALWAYS');
+            });
+        }
+    },
+}
